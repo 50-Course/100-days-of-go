d047bdf13d737811a30f142ebf0e7ad6359cc775
50-Course <erioluwaadeodu@my.uopeople.edu>
2025-01-08 14:27:40 +0100

    chore: update test suite with placeholders

--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -8,11 +8,11 @@ import "testing"
 // does not exist in Go, instead there are interface which as as the topmost-level
 // 'interface' (or let me call it object) for now, of which all other types, or 'objects'
 // inherits from
-func assertEqual(t testing.TB, got interface{}, want int) {
+func assertEqual(t testing.TB, got, want int) {
 	t.Helper() // acts as python's functools.wraps
 
 	if got != want {
-		t.Errorf("AssertionError: expected %d for an answer but received %d, given %v", want, got, nums)
+		t.Errorf("AssertionError: expected %d for an answer but received %d", want, got)
 	}
 }
 
@@ -22,12 +22,16 @@ func TestSum(t *testing.T) {
 	got := Sum(nums)
 	want := 15
 
-	// t.Run("Array of of 5 integers results in the expected value", func(t *testing.T) {
-	// })
-	// t.Run("Array of values greater than capacity of five results in an error", func(t *testing.T) {
-	// })
-	// t.Run("Arrays of a combination of postive and negative integers results in an expected value", func(t *testing.T) {
-	// })
-	// t.Run("Array of all negative values added together results in a negative value", func(t *testing.T) {
-	// })
+	t.Run("Array of of 5 integers results in the expected value", func(t *testing.T) {
+		assertEqual(t, got, want)
+	})
+	t.Run("Array of values greater than capacity of five results in an error", func(t *testing.T) {
+		assertEqual(t, got, want)
+	})
+	t.Run("Arrays of a combination of postive and negative integers results in an expected value", func(t *testing.T) {
+		assertEqual(t, got, want)
+	})
+	t.Run("Array of all negative values added together results in a negative value", func(t *testing.T) {
+		assertEqual(t, got, want)
+	})
 }
