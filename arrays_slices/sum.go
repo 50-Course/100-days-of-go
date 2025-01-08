9a544048de48697b75710b3bac5f5b63ac330142
50-Course <erioluwaadeodu@my.uopeople.edu>
2025-01-08 14:43:50 +0100

    chore: minor updates

--- arrays_slices/sum.go
+++ arrays_slices/sum.go
@@ -1,7 +1,15 @@
 package arrays_slices
 
+import (
+	_ "errors"
+	_ "fmt"
+)
+
 func Sum(arr [5]int) int {
 	count := 0
+
+	if len(arr) > 5 {
+	}
 
 	for _, n := range arr {
 		count += n
--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -1,6 +1,8 @@
 package arrays_slices
 
-import "testing"
+import (
+	"testing"
+)
 
 // so my understanding now is all objects inherit from the `interface` 'Object'
 //
@@ -17,21 +19,40 @@ func assertEqual(t testing.TB, got, want int) {
 }
 
 func TestSum(t *testing.T) {
-	nums := [5]int{1, 2, 3, 4, 5}
-
-	got := Sum(nums)
-	want := 15
-
 	t.Run("Array of of 5 integers results in the expected value", func(t *testing.T) {
+		nums := [5]int{1, 2, 3, 4, 5}
+
+		got := Sum(nums)
+		want := 15
 		assertEqual(t, got, want)
 	})
-	t.Run("Array of values greater than capacity of five results in an error", func(t *testing.T) {
-		assertEqual(t, got, want)
-	})
+	// t.Run("Array of values greater than capacity of five results in an error", func(t *testing.T) {
+	// 	numsArrGreaterThanSizeFive := [...]int{1, 2, 3, 4, 5, 6}
+	//
+	// 	got := Sum(numsArrGreaterThanSizeFive)
+	// 	want := assertEqual(t, got, want)
+	// })
 	t.Run("Arrays of a combination of postive and negative integers results in an expected value", func(t *testing.T) {
+		numsCombo := [...]int{1, -2, 3, -4, -5}
+
+		got := Sum(numsCombo)
+		// expected = -7
+		want := -7
+
 		assertEqual(t, got, want)
 	})
 	t.Run("Array of all negative values added together results in a negative value", func(t *testing.T) {
+		var negativeNums [5]int
+
+		negativeNums[0] = -1
+		negativeNums[0] = -2
+		negativeNums[0] = -3
+		negativeNums[0] = -4
+		negativeNums[0] = -5
+
+		got := Sum(negativeNums)
+		// expected = -15
+		want := -15
 		assertEqual(t, got, want)
 	})
 }
